41ca140c3fee826384a7a74e21022c98c0fa9999
“pengzhiliang <1083127130@qq.com>
2018-10-24 15:20:19 +0800

    delete other dataset loader

---
+++ datasets/__init__.py
@@ -0,0 +1,24 @@
+import json
+
+from pascal_voc import pascalVOCLoader
+
+
+
+def get_loader(name):
+    """get_loader
+
+    :param name:
+    """
+    return {
+        "pascal": pascalVOCLoader,
+    }[name]
+
+
+def get_data_path(name, config_file="datapath.json"):
+    """get_data_path
+
+    :param name:
+    :param config_file:
+    """
+    data = json.load(open(config_file))
+    return data[name]["data_path"]
